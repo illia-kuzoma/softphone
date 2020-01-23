7593e3d34e3d26d8c05beb0adf0e5401133e1a45
Illia Kuzoma <illia.kuzoma@wellnessliving.com>
2020-01-23 11:46:36 +0200

    update ReportMissed - getAll

--- app/Http/Controllers/SoftPhone/ReportMissed.php
+++ app/Http/Controllers/SoftPhone/ReportMissed.php
@@ -18,7 +18,39 @@ class ReportMissed extends Controller
     {
 //        $missedCalls = MissedCall::all();
 //        return view('report.missed.index', compact('missedCalls'));
-        return view('report.missed.index');
+//        return view('report.missed.index');
+        $out = [
+            'diagrama' => [
+                'user_id' => '1',
+                'first_name' => 'Ivan',
+                'last_name' => 'Petrov',
+                'calls_count' => 0,
+            ],
+            'calls' => [
+                'data' => [
+                    'user_id' => '1',
+                    'photo_url' => 'https://www.bmw-motorsport.com/content/dam/bmw/marketBMWSPORTS/bmw-motorsport_com/assets/bmw-m-motorsport/race-cars/bmw-m2-cs-racing/bmw-m2-cs-racing-ascari-hotspot.jpg',
+                    'first_name' => 'Ivan',
+                    'last_name' => 'Petrov',
+                    'business_name' => 'Bavaria Motors LLC',
+                    'contact' => 'UFO',
+                    'priority' => 'low',
+                    'phone' => '+380508008080',
+                    'time_create' => '1579996800',
+                ],
+                'page_count' => 1,
+                'page' => 1
+            ],
+            'user' => [
+                'user_id' => 1,
+                'photo_url' => 'https://www.bmw-motorsport.com/content/dam/bmw/marketBMWSPORTS/bmw-motorsport_com/assets/bmw-m-motorsport/race-cars/bmw-m2-cs-racing/bmw-m2-cs-racing-ascari-hotspot.jpg',
+                'first_name' => 'Ivan',
+                'last_name' => 'Petrov',
+                'role' => 'user'
+            ],
+        ];
+
+        return json_encode($out);
     }
 
     /**
