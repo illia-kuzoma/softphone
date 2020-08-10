3b76ed5777fc6d3cf46270b1e919e18d25dbc9b5
AndriiVMWork <andriimuzika@gmail.com>
2020-08-10 17:54:08 +0300

    * Changed host.

--- vuejs/src/views/Dashboard.vue
+++ vuejs/src/views/Dashboard.vue
@@ -391,7 +391,7 @@ softphone
         }
         else {
           this.$loading(true);
-          HttpService.methods.get('http://softphone/report/missed/'+(refresh?'refresh/':'')+token)
+          HttpService.methods.get('http://callcentr.wellnessliving.com/report/missed/'+(refresh?'refresh/':'')+token)
           .then(function (response) {
             self.$loading(false);
             if(response.data.error===true){
@@ -460,7 +460,7 @@ softphone
 
         this.$loading(true);
         HttpService.methods.get(
-          'http://softphone/report/missed/call/'+
+          'http://callcentr.wellnessliving.com/report/missed/call/'+
           startDate + '/' +
           period + '/' +
           uid + '/' +
@@ -488,7 +488,7 @@ softphone
           ss_agent_id = "/" + self.s_agent_id
         }
         this.$loading(true);
-        HttpService.methods.get('http://softphone/report/missed/call/'+
+        HttpService.methods.get('http://callcentr.wellnessliving.com/report/missed/call/'+
           startDate + '/' + period + ss_agent_id)
         .then(function (response) {
           self.$loading(false);
@@ -502,7 +502,7 @@ softphone
       },
       getTableData(){
         var self = this;
-        HttpService.methods.get('http://softphone/report/missed/call')
+        HttpService.methods.get('http://callcentr.wellnessliving.com/report/missed/call')
         .then(function (response) {
           let tableData = response.data.calls
           self.setTableData(tableData);
