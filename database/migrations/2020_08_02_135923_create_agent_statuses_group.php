670db30a662a66cfe38012e885464815f31891bf
AndriiVMWork <andriimuzika@gmail.com>
2020-08-26 23:05:43 +0300

    * fixes of table structure

--- database/migrations/2020_08_02_135923_create_agent_statuses_group.php
+++ database/migrations/2020_08_02_135923_create_agent_statuses_group.php
@@ -17,8 +17,8 @@ class CreateAgentStatusesGroup extends Migration
             $table->unsignedBigInteger('agent_id');
             $table->string('status_name', 100);
             $table->string('status_value', 100);
-            $table->timestamp('time_start', 0)->comment("Time enter in status.");
-            $table->timestamp('time_end', 0)->comment("Time leave out status.");
+            $table->timestamp('time_start', 0)->nullable()->comment("Time enter in status.");
+            $table->timestamp('time_end', 0)->nullable()->comment("Time leave out status.");
             $table->timestamps();
             $table->unique(['agent_id', 'status_name', 'time_start'], 'report_agent_statuses_groups__uniq_key');
         });
